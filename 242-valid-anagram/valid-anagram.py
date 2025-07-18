dfbb062b2772a9bb4a0180ee1c791737eeea0254
Upamanyu <151928697+smupamanyu@users.noreply.github.com>
2025-07-18 13:26:17 +0530

    Time: 21 ms (60.87%) | Memory: 14.3 MB (15.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,13 @@
-from collections import Counter
-
 class Solution(object):
     def isAnagram(self, s, t):
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        count_letters_s = {}
+        count_letters_t = {}
+
+        for i in range(len(s)):
+            count_letters_s[s[i]] = count_letters_s.get(s[i], 0) + 1
+            count_letters_t[t[i]] = count_letters_t.get(t[i], 0) + 1
+
+        return count_letters_s == count_letters_t
